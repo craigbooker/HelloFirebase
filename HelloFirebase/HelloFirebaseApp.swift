23a4b534b55ada232c85bbebaf9f43b194dc65a4
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-10-25 11:28:12 -0500

    Initial Commit

---
+++ HelloFirebase/HelloFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFirebaseApp.swift
+//  HelloFirebase
+//
+//  Created by Craig Booker on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
